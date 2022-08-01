448219b034a098ee7f38076850b2bbd8fc2c9f4d
Arty Tairebien <artemtirabian@citadel.one>
2022-08-02 00:52:17 +0200

    Deploy scripts and links added

---
+++ build/precache-manifest.c97aaa2ab57f08eaef96103426577321.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451dca2b98fdb2321f3b2c34a060bcfe",
+    "url": "/cicd_learning/index.html"
+  },
+  {
+    "revision": "d3145772530c88538a8d",
+    "url": "/cicd_learning/static/css/main.62008f7a.chunk.css"
+  },
+  {
+    "revision": "7a8ee02b5e901e60295a",
+    "url": "/cicd_learning/static/js/2.b56799b3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/cicd_learning/static/js/2.b56799b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3145772530c88538a8d",
+    "url": "/cicd_learning/static/js/main.39309485.chunk.js"
+  },
+  {
+    "revision": "a1b605e8bcd2d742c79a",
+    "url": "/cicd_learning/static/js/runtime-main.46c3213e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/cicd_learning/static/media/image.d7265326.png"
+  }
+]);
